9de7b9c874dd2175cd1f92880bbf1467779d4511
Mohamed Ismaiel Ahmed <mohamed.ismaiel@hotmail.com>
2024-05-03 14:55:14 -0600

    upload files

---
+++ PART1_ETSCIcalculation/ClimpactSoftwareFiles/climpact-master/app.R
@@ -0,0 +1,118 @@
+# ------------------------------------------------
+# Climpact
+# University of New South Wales
+# This package is available on github https://github.com/ARCCSS-extremes/climpact
+# ------------------------------------------------
+#
+# This file constitutes the main user entry point into Climpact.
+#
+# BUGS
+#   - Currently SPEI/SPI are calculated via the old Climpact code. This is because the CRAN package for SPEI/SPI does not
+#     ostenisbly support large runs of NA values. When this occurs real numbers are included in the output where NA values
+#     should occur.
+#
+# TECHNICAL NOTES
+#   - See server/Climpact_developer_documentation.md for a technical document describing the major parts of this code. Refer
+#     to this for insight when making changes such as adding indices.
+#
+# HISTORY
+#   The Climpact code has evolved very heavily since it's original incarnation in the form of the single source file climpact.r.
+#   The significant changes that have taken place include;
+#   - the calculation of the indices is almost entirely taken care of by climdex.pcic (with several exceptions such as the
+#     heatwave indices and SPEI/SPI).
+#   - the user interface has been re-written with R Shiny
+#   - the indices can now be calculated on netCDF files.
+#
+#   Several people contributed significantly to the development of the original Climpact software (which was originally derived
+#   from RClimdex). For posterity and credit, below is a list of the key names and dates attributed to different changes.
+#
+#   Programmed by Yujun Ouyang,Mar,2004
+#   rewritten by Yang Feng, July 2004
+#   version 1.0, 2004-10-14
+#   modified, 2006-01-24
+#   modified, 2007-03-23
+#   modified, 2007-11-26
+#   modified, 2008-05-05
+#   modified, 2008-05-06
+#   modified, 2008-06-16
+#   modified, 2012-05-30
+#   Sandra add new indices
+#   Hongang to check Sandra's code and add new indices - from 2012-11-05
+#   modified 2013, James Goldie - overhaul of code
+#   overhaul 2015-2017, Nicholas Herold and Nicholas Hannah (PCIC R package implementation, R Shiny interface, gridded indices, new indices).
+
+
+# Load libraries
+# Note, other libraries are sourced at different points in the program depending on what functions the user interacts with.
+library(zoo)
+library(zyp)
+library(tcltk2)
+library(parallel)
+library(shinythemes)
+library(shinyjs)
+library(markdown)
+library(servr)
+library(dplyr)
+library(corrplot)
+library(ggplot2)
+library(shinydashboard)
+library(shinyBS)
+library(slickR)
+library(shinyWidgets)
+library(qpdf)
+
+# If Windows then use rbase file and directory chooser, if Unix use tcltk file and directory chooser. 
+# Base functions do not provide a dialog box in Unix environments.
+if (.Platform$OS.type == "windows") {
+  fchoose <<- get("choose.files", mode = "function")
+  dchoose <<- get("choose.dir", mode = "function")
+} else if (.Platform$OS.type == "unix") {
+  fchoose <<- get("tk_choose.files", mode = "function")
+  dchoose <<- get("tk_choose.dir", mode = "function")
+}
+
+isLocal <<- FALSE
+if (Sys.getenv("SHINY_PORT") == "") {
+  isLocal <<- TRUE
+}
+
+# Source files
+source("server/climpact.GUI-functions.r")
+package.check()
+source("server/climpact.etsci-functions.r")
+source("server/sector_correlation.R")
+source("models/errors.R")
+source("models/climdexInputParams.R")
+source("models/ncdfInputParams.R")
+source("models/ncdfThresholdInputParams.R")
+source("models/sectorInputParams.R")
+source("models/weatherObservationsRequest.R")
+source("models/weatherStation.R")
+source("modules/singleStationStep1.R")
+source("modules/singleStationStep1UI.R")
+source("modules/singleStationStep2.R")
+source("modules/singleStationStep2UI.R")
+source("modules/singleStationStep3.R")
+source("modules/singleStationStep3UI.R")
+source("modules/singleStationStep4.R")
+source("modules/singleStationStep4UI.R")
+source("modules/batchStep1.R")
+source("modules/batchStep1UI.R")
+source("modules/griddedStep1.R")
+source("modules/griddedStep1UI.R")
+source("modules/griddedStep2.R")
+source("modules/griddedStep2UI.R")
+source("server/server.R")
+source("services/zipFiles.R")
+source("ui/uiSupport.R")
+source("ui/ui.R")
+source("server/global_variables.r")
+
+if (isLocal) {
+    # When running locally, support NetCDF file uploads and gridded data calculations
+  # Increase file upload limit to something extreme to account for large files.
+  options(shiny.maxRequestSize = 1000000 * 1024 ^ 2)
+  ncFilter <<- matrix(c("NetCDF", "*.nc"), 1, 2, byrow = TRUE)
+}
+
+shinyApp(ui, server)
---
+++ PART1_ETSCIcalculation/ClimpactSoftwareFiles/climpact-master/climpact.batch.stations.r
@@ -0,0 +1,123 @@
+# ------------------------------------------------
+# This file contains functionality to batch process station files stored in "RClimdex format" (i.e. 6 column format, see www/sample_data/sydney_observatory_hill_1936-2015.txt for an example)
+# ------------------------------------------------
+#
+# CALLING THIS FILE:
+#    Rscript climpact.batch.stations.r /full/path/to/station/files/ /full/path/to/metadata.txt base_period_begin base_period_end cores_to_use
+#    e.g. Rscript climpact.batch.stations.r ./www/sample_data/Renovados_hasta_2010 ./www/sample_data/climpact.sample.batch.metadata.txt 1971 2000 2
+#
+# NOTE: This file expects that all of your individual station files are kept in /directory/path/to/station/files/ and that each file name (excluding the path directory) is found in
+#       column one of metadata.txt, with corresponding metadata in subsequent columns.
+#
+# COMMAND LINE FLAGS:
+#     - /full/path/to/station/files/ : directory path to where files listed in column one of metadata.txt are kept.
+#     - /full/path/to/metadata.txt : text file containing 12 columns; station file, latitude, longitude, wsdin, csdin, Tb_HDD, Tb_CDD, Tb_GDD, rx_ud, rnnmm_ud, txtn_ud, SPEI
+#     - base_period_begin : beginning year of base period
+#     - base_period_end : end year of base period
+#     - cores_to_use : number of cores to use in parallel
+
+# load and source and specify cores
+library(foreach)
+library(doSNOW)
+library(climdex.pcic)
+library(doParallel)
+library(zoo)
+library(zyp)
+library(qpdf)
+
+source("models/climdexInputParams.R")
+source("server/climpact.GUI-functions.r")
+source("server/climpact.etsci-functions.r")
+source("server/global_variables.r")
+source("server/batch_stations.R")
+source("services/quality_control_checks.R")
+
+# return station metadata
+read.file.list.metadata <- function(metadata_filepath) {
+    metadataTable <- read.table(metadata_filepath,
+    header = TRUE,
+    col.names = c("station_file", "latitude", "longitude", "wsdin",   "csdin",   "Tb_HDD", "Tb_CDD", "Tb_GDD", "rxnday",  "rnnmm", "txtn", "SPEI"),
+    colClasses = c("character",   "real",     "real",      "integer", "integer", "real",   "real",   "real",   "integer", "real",  "real", "integer"))
+  return(metadataTable)
+}
+
+strip_file_extension <- function(fileName) {
+  file_parts <- strsplit(fileName, "\\.")[[1]]
+  stripped <- substr(fileName, start = 1, stop = nchar(fileName) - nchar(file_parts[length(file_parts)]) - 1)
+  return(stripped)
+}
+
+# call QC and index calculation functionality for each file specified in metadata.txt
+batch <- function(input.directory,file.list.metadata,base.start,base.end) {
+	batchMode <<- TRUE
+	version.climpact <<- software_id
+	temp.quantiles <<- c(0.05, 0.1, 0.5, 0.9, 0.95)
+	prec.quantiles <<- c(0.05, 0.1, 0.5, 0.9, 0.95, 0.99)
+	barplot_flag <<- TRUE
+	min_trend <<- 10
+
+	metadata <- read.file.list.metadata(file.list.metadata)
+	
+	if(exists("progress") && !is.null(progress)) { 
+		prog_int <- 0.5/length(metadata$station_file) 
+	}
+	
+	progressSNOW <- function(n) { 
+		if(interactive()) { progress$inc(prog_int) }
+	}
+	opts <- list(progress = progressSNOW)
+
+	# create batchFiles data frame, with cols name, datapath
+	inputContents <- list.files(input.directory, full.names = TRUE)
+	batchFiles <- data.frame(name = basename(inputContents), datapath = inputContents, stringsAsFactors = FALSE)
+	batchOutputFolder <- strip_file_extension(file.list.metadata)
+	print(paste0("Processing batch, to output folder: ", batchOutputFolder))
+	processBatch(opts$progress(), file.list.metadata, batchFiles, base.start, base.end, batchOutputFolder)
+
+	print("",quote=FALSE)
+	print("",quote=FALSE)
+	print("",quote=FALSE)
+	print("",quote=FALSE)
+	print("",quote=FALSE)
+	print("",quote=FALSE)
+	print("*********************************************************************************************",quote=FALSE)
+	print("*********************************************************************************************",quote=FALSE)
+	print("*********************************************************************************************",quote=FALSE)
+	print("PROCESSING COMPLETE.",quote=FALSE)
+	print("",quote=FALSE)
+	print("",quote=FALSE)
+	print("",quote=FALSE)
+	print("Any log files to do with errors encountered during processing are listed below. Examine these files carefully and correct any errors.",quote=FALSE)
+	print("",quote=FALSE)
+	error.files <- suppressWarnings(list.files(path=batchOutputFolder,pattern="*errors.txt|*missing_dates*",recursive = TRUE,full.names=TRUE))
+    #error.files <- suppressWarnings(list.files(path=file.path("www","output"),pattern="*errors.txt|*missing_dates*",recursive = TRUE,full.names=TRUE))
+	if(length(error.files)==0) { print("... no errors detected in processing your files. That doesn't mean there aren't any!",quote=FALSE) } 
+	else {
+		for (i in 1:length(error.files)) { #system(paste("ls ",input.directory,"*error.txt | wc -l",sep=""))) {
+			print(paste0(error.files[i]),quote=FALSE)
+		} 
+	}
+}
+
+# set up variables and call main function if this is from the command line
+if(!interactive()) {
+  # Enable reading of command line arguments
+  args<-commandArgs(TRUE)
+  
+  # where one or more station files are kept
+  input.directory = toString(args[1])
+  
+  # metadata text file
+  file.list.metadata = toString(args[2])
+  
+  # begin base period
+  base.start = as.numeric(args[3])
+  
+  # end base period
+  base.end = as.numeric(args[4])
+  
+  # establish multiple cores
+  registerDoParallel(cores=as.numeric(args[5]))
+  
+  batch(input.directory,file.list.metadata,base.start,base.end)
+}
